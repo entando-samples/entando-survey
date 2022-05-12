0b786839047a4455fc18d68b2be659f245aff3ce
Gokul Khatiwada <gokulkhatiwada@Gokuls-MacBook-Pro.local>
2022-05-12 20:34:56 +0545

    auth keycloak added

--- ui/widgets/widgets-dir/ent-survey-widget/resources/js/ent-survey.js
+++ ui/widgets/widgets-dir/ent-survey-widget/resources/js/ent-survey.js
@@ -2,24 +2,79 @@ import Vue from './config'
 import { createPinia} from 'pinia';
 import App from './App';
 import router from './router';
+// import Keycloak from 'keycloak-js';
+
+let initOptions = {
+    url: 'http://localhost:9080/auth',
+    realm: 'entando',
+    clientId: 'web_app',
+    onLoad: 'login-required'
+}
+
+// let keycloak = Keycloak(initOptions);
 
 
 
 
 class EntSurvey extends HTMLElement {
+
     connectedCallback() {
         this.mountPoint = document.createElement('span')
         this.render()
     }
 
+
+
     render() {
+        // debugger
+        // keycloak.init({ onLoad: initOptions.onLoad })
+        //     .success((auth) => {
+        //     debugger
+        //     console.log(auth);
+        //     if (!auth) {
+        //         window.location.reload();
+        //     } else {
+        //        console.log("Authenticated");
+        //        console.log(keycloak.token);
+
         new Vue({
             el: this.appendChild(this.mountPoint),
             render: (h) => h(App),
             pinia: createPinia(),
             router,
         });
-    }
+            // }
+
+
+//Token Refresh
+//             setInterval(() => {
+//                 keycloak.updateToken(70).then((refreshed) => {
+//                     if (refreshed) {
+//                        console.log('Token refreshed' + refreshed);
+//                     } else {
+//                         console.log('Token not refreshed, valid for '
+//                             + Math.round(keycloak.tokenParsed.exp + keycloak.timeSkew - new Date().getTime() / 1000) + ' seconds');
+//                     }
+//                 }).catch(() => {
+//                     console.log('Failed to refresh token');
+//                 });
+//             }, 6000)
+
+        };
+        //     .catch(() => {
+        //    console.log("Authenticated Failed");
+        // });
+
+
+
+
+
+
+
+
+
+
+    // }
 }
 
 customElements.get('ent-survey-widget') || customElements.define("ent-survey-widget", EntSurvey)
