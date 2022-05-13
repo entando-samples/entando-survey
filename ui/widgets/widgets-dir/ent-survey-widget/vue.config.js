6b2589da36285827be5d91ab2e293851d5c7a990
GermanoGiudici <germano.giudici@gmail.com>
2022-05-13 16:44:13 +0200

    compilation ok

--- ui/widgets/widgets-dir/ent-survey-widget/vue.config.js
+++ ui/widgets/widgets-dir/ent-survey-widget/vue.config.js
@@ -1,12 +1,31 @@
-const { defineConfig } = require('@vue/cli-service')
+const {defineConfig} = require('@vue/cli-service')
+const appPackageJson = require('./package.json');
+const webpack = require('webpack');
 const path = require("path");
+
+
+
 module.exports = defineConfig({
-  transpileDependencies: true,
-  configureWebpack: {
-  resolve: {
-    alias: {
-      "@": path.join(__dirname, "src/js"),
+    transpileDependencies: true,
+    assetsDir: 'static',
+    outputDir: 'build',
+    //publicPath: '/entando-de-app/cmsresources/ent-prj-tmpl-vue-bundle/',
+    configureWebpack: config => {
+        const filename = `${appPackageJson.name}-${appPackageJson.version}`;
+        config.plugins.push(new webpack.optimize.LimitChunkCountPlugin({
+            maxChunks: 1
+        }));
+
+        return {
+            output:{
+                filename: `static/js/${filename}.js`,
+            },
+            resolve: {
+                alias: {
+                    "@": path.join(__dirname, "src/js"),
+                }
+            }
+        }
     }
-  }
-}
+
 })
