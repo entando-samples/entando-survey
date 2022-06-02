da7832e28c1dc09df39d1e3e65ad9027e39c5b1c
Pierluca Lino <pierluca.lino@kotuko.it>
2022-06-02 10:54:25 +0200

    Remove seeders not used

--- src/main/laravel/database/seeders/DatabaseSeeder.php
+++ src/main/laravel/database/seeders/DatabaseSeeder.php
@@ -8,16 +8,7 @@ class DatabaseSeeder extends Seeder
 {
     public function run()
     {
-        $this->call(AppSettingSeeder::class);
-        $this->call(UserSeeder::class);
-        $this->call(PathologySeeder::class);
-        $this->call(DocumentSeeder::class);
-        $this->call(FaqSeeder::class);
         $this->call(QuestionSeeder::class);
         $this->call(SurveySeeder::class);
-        $this->call(MessageSeeder::class);
-        $this->call(LinkDocumentToPatientSeeder::class);
-        $this->call(PatientDocumentSeeder::class);
-        $this->call(LinkSurveyToPatientSeeder::class);
     }
 }
