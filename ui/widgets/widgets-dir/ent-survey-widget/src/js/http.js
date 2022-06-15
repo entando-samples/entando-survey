e23dc1d2854cf623009d0cc3bdbf100840e49d59
Pierluca Lino <pierluca.lino@kotuko.it>
2022-06-15 20:13:13 +0200

    Refresh token implemented

--- ui/widgets/widgets-dir/ent-survey-widget/src/js/http.js
+++ ui/widgets/widgets-dir/ent-survey-widget/src/js/http.js
@@ -8,16 +8,23 @@ const client = axios.create({
 
 window.addEventListener('keycloak', function (e) {
     // console.log(e);
-    const useAuthStore = useAuth();
-    console.log(entando.keycloak.token);
-    client.interceptors.request.use(request => {
-        request.headers.common['Accept'] = 'application/json';
-        request.headers.common['Content-Type'] = 'application/json';
-        request.headers.common['Authorization'] = `Bearer ${entando.keycloak.token}`;
+    // console.log(e.detail.eventType);
+    if (e.detail.eventType === 'onTokenExpired') {
+        entando.keycloak.updateToken(300);
+    }
+    if (e.detail.eventType === 'onReady' || 
+        e.detail.eventType === 'onAuthRefreshSuccess') {
+        client.interceptors.request.use(request => {
+            request.headers.common['Accept'] = 'application/json';
+            request.headers.common['Content-Type'] = 'application/json';
+            request.headers.common['Authorization'] = `Bearer ${entando.keycloak.token}`;
 
-        return request;
-    });
-    useAuthStore.getUser().then(r => console.log(r));
+            return request;
+        });
+    }
+    // TODO: Update this for keycloak.
+    // const useAuthStore = useAuth();
+    // useAuthStore.getUser().then(r => console.log(r));
 });
 
 client.interceptors.response.use(
