4c8d72e7360f9dfe631fb2db7a20ff42834fb7a6
Simone Leo <simone.leo@kotuko.it>
2022-06-22 16:21:25 +0200

    add form to question creation

--- ui/widgets/widgets-dir/ent-survey-widget/src/js/views/questions/Form.vue
+++ ui/widgets/widgets-dir/ent-survey-widget/src/js/views/questions/Form.vue
@@ -1,27 +1,100 @@
 <template>
     <div class="mt-5">
         <b-page-header title="Crea nuovo documento" />
-        <form class="bg-white p-4 rounded-md w-full" @submit.prevent="save">
-            <div>
-                <label for="json" class="font-semibold">JSON</label>
+        <form class="bg-white p-4 rounded-md w-full">
+            <div class="question-it">
+                <label for="questionIt" class="font-semibold">Domanda (ITA)</label>
                 <div class="mt-1">
-                    <textarea
+                    <input 
+                        v-model="form.localizedQuestion[0].question"
+                        type="text"
                         class="w-full border border-primary px-2 py-1 focus-within:outline-none"
-                        v-model="form.json"
-                        id="json"
-                        rows="10"
-                        placeholder="paste json here..."
-                    ></textarea>
-                    <err-msg :message="errors.json"></err-msg>
+                        id="questionIt"
+                    />
                 </div>
             </div>
-            <b-button class="mt-3" :disabled="loading">Save</b-button>
+            <div class="question-description-it mt-2">
+                <label for="questionDescriptionIt" class="font-semibold">Descrizione domanda (ITA)</label>
+                <div class="mt-1">
+                    <input 
+                        v-model="form.localizedQuestionDescription[0].description"
+                        type="text"
+                        class="w-full border border-primary px-2 py-1 focus-within:outline-none"
+                        id="questionDescriptionIt"
+                    />
+                </div>
+            </div>
+            <div class="answers-it mt-2">
+                <label for="answersIt" class="font-semibold">Risposte (ITA)</label>
+                <div class="mt-1 flex flex-col">
+                    <div class="flex">
+                        <input 
+                            v-model="localizedAnswersIt"
+                            type="text"
+                            class="border border-primary px-2 py-1 focus-within:outline-none"
+                            id="answersIt"
+                        />
+                        <b-button class="ml-4" :disabled="!localizedAnswersIt" @click.prevent="addItAnswer">Aggiungi possibile risposta</b-button>
+                    </div>
+                    <div v-if="form.localizedAnswers[0].answers.length" class="mt-1 flex flex-col">
+                        <span>Possibili risposte:</span>
+                        <span v-for="(answer, index) in form.localizedAnswers[0].answers" :key="index">
+                            {{ `${index + 1} - ${answer}` }}
+                        </span>
+                    </div>
+                </div>
+            </div>
+            <div class="question-en mt-2">
+                <label for="questionEn" class="font-semibold">Question (ENG)</label>
+                <div class="mt-1">
+                    <input 
+                        v-model="form.localizedQuestion[1].question"
+                        type="text"
+                        class="w-full border border-primary px-2 py-1 focus-within:outline-none"
+                        id="questionEn"
+                    />
+                </div>
+            </div>
+            <div class="question-description-en mt-2">
+                <label for="questionDescriptionEn" class="font-semibold">Question's description (ENG)</label>
+                <div class="mt-1">
+                    <input 
+                        v-model="form.localizedQuestionDescription[1].description"
+                        type="text"
+                        class="w-full border border-primary px-2 py-1 focus-within:outline-none"
+                        id="questionDescriptionEn"
+                    />
+                </div>
+            </div>
+            <div class="answers-en mt-2">
+                <label for="answersEn" class="font-semibold">Answers (ENG)</label>
+                <div class="mt-1 flex flex-col">
+                    <div class="flex">
+                        <input 
+                            v-model="localizedAnswersEn"
+                            type="text"
+                            class="border border-primary px-2 py-1 focus-within:outline-none"
+                            id="answersEn"
+                        />
+                        <b-button class="ml-4" :disabled="!localizedAnswersEn" @click.prevent="addEnAnswer">Add possible answer</b-button>
+                    </div>
+                    <div v-if="form.localizedAnswers[1].answers.length" class="mt-1 flex flex-col">
+                        <span>Possible answers:</span>
+                        <span v-for="(answer, index) in form.localizedAnswers[1].answers" :key="index">
+                            {{ `${index + 1} - ${answer}` }}
+                        </span>
+                    </div>
+                </div>
+            </div>
+            <div class="mt-3 flex flex-row-reverse">
+                <b-button @click.prevent="save" :disabled="loading">Save</b-button>
+            </div>
         </form>
     </div>
 </template>
 
 <script>
-import { defineComponent, reactive } from '@vue/composition-api';
+import { defineComponent, reactive, ref } from '@vue/composition-api';
 import useQuestions from '@/compositions/questions';
 import Vue from 'vue';
 import router from '@/router';
@@ -30,23 +103,61 @@ export default defineComponent({
     name: "QuestionFormPage",
     setup(props, { root }) {
         const form = reactive({
-            json: ''
+            localizedQuestion: [
+                { question: '', lng: 'ita' },
+                { question: '', lng: 'eng' },
+            ],
+            localizedQuestionDescription: [
+                { description: '', lng: 'ita' },
+                { description: '', lng: 'eng' },
+            ],
+            localizedAnswers: [
+                { answers: [], lng: 'ita' },
+                { answers: [], lng: 'eng' },
+            ],
+            primaryKey: '',
+            order: '',
+            protocolQuestion: '',
+            scores: [],
+            protocol: ''
         });
+        
+        let localizedAnswersIt = ref('');
+        let localizedAnswersEn = ref('');
+        
         const { errors, loading, saveQuestion } = useQuestions();
 
         function save() {
+            console.log(form)
             saveQuestion(form).then(({ message }) => {
                 Vue.toasted.success(message);
-
                 router.replace('/questions');
             })
+        }
+
+        function addItAnswer() {
+            if (localizedAnswersIt.value) {
+                form.localizedAnswers[0].answers.push(localizedAnswersIt.value)
+                localizedAnswersIt.value = ''
+            }
+        }
+        
+        function addEnAnswer() {
+            if (localizedAnswersEn.value) {
+                form.localizedAnswers[1].answers.push(localizedAnswersEn.value)
+                localizedAnswersEn.value = ''
+            }
         }
 
         return {
             errors,
             form,
             loading,
-            save
+            localizedAnswersIt,
+            localizedAnswersEn,
+            save,
+            addItAnswer,
+            addEnAnswer,
         }
     }
 })
