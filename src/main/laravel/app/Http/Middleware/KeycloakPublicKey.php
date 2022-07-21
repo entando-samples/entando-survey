bbd03caf6c7bcf5fb8c7b9e47871622d99e065d1
Pierluca Lino <pierluca.lino@kotuko.it>
2022-07-21 14:47:53 +0200

    Select the proper keycloak certificate

--- src/main/laravel/app/Http/Middleware/KeycloakPublicKey.php
+++ src/main/laravel/app/Http/Middleware/KeycloakPublicKey.php
@@ -10,10 +10,13 @@ class KeycloakPublicKey
 {
     public function handle(Request $request, \Closure $next)
     {
+        $auth = explode(' ', $request->header('Authorization'));
+        $decoded_jwt = array_map('base64_decode', explode('.', $auth[1]));
+        $params = json_decode($decoded_jwt[0], true);
 
         $keycloakAuthService = new KeycloakAuthService();
         try{
-            $publicKey = $keycloakAuthService->handle();
+            $publicKey = $keycloakAuthService->handle($params);
 
             config(['keycloak.realm_public_key'=>$publicKey]);
             return $next($request);
