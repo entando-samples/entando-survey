43b53c3a7dcdfa576b96be5cc50ade99404ef12a
Gokul Khatiwada <gokulkhatiwada@Gokuls-MacBook-Pro.local>
2022-07-22 15:39:45 +0545

    remvoved not required classes

--- src/main/laravel/app/Http/Controllers/Api/V1/Backend/UserController.php
+++ src/main/laravel/app/Http/Controllers/Api/V1/Backend/UserController.php
@@ -2,14 +2,10 @@
 
 namespace App\Http\Controllers\Api\V1\Backend;
 
-use App\Models\User;
+
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Str;
-use Illuminate\Http\Request;
-use App\Http\Requests\UserRequest;
+
 use App\Http\Controllers\Controller;
-use App\Http\Resources\UserResource;
-use App\Jobs\SendPasswordResetEmail;
 
 class UserController extends Controller
 {
