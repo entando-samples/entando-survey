f869a760c2ed7c29adc282889aaa9f56b2306489
GermanoGiudici <germano.giudici@gmail.com>
2022-08-26 16:31:26 +0200

    removed login button

--- ui/widgets/widgets-dir/ent-survey-responses-widget/src/js/views/layouts/App/Header.vue
+++ ui/widgets/widgets-dir/ent-survey-responses-widget/src/js/views/layouts/App/Header.vue
@@ -1,48 +1,50 @@
 <template>
   <header
-    class="md:pl-sidebar h-header flex items-center shadow-md w-full z-10"
+      class="md:pl-sidebar h-header flex items-center shadow-md w-full z-10"
   >
     <div class="w-full container mx-auto px-4">
       <div class="w-full flex items-center justify-between">
         <portal-target name="header"></portal-target>
 
-        <div class="flex items-center space-x-5 text-primary">
-          <div class="flex items-center">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-7 w-7 fill-current"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z"
-                clip-rule="evenodd"
-              />
-            </svg>
-            <!--drop down -->
-            <div>
-              <v-dropdown placement="right">
-                <!-- Button content -->
-                <template v-slot:button>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    class="h-5 w-5 fill-current"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                      clip-rule="evenodd"
-                    />
-                  </svg>
-                </template>
-              </v-dropdown>
-            </div>
-            <!--drop down ends -->
-          </div>
-        </div>
+        <!--
+                <div class="flex items-center space-x-5 text-primary">
+                  <div class="flex items-center">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="h-7 w-7 fill-current"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z"
+                        clip-rule="evenodd"
+                      />
+                    </svg>
+                    &lt;!&ndash;drop down &ndash;&gt;
+                    <div>
+                      <v-dropdown placement="right">
+                        &lt;!&ndash; Button content &ndash;&gt;
+                        <template v-slot:button>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            class="h-5 w-5 fill-current"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                          >
+                            <path
+                              fill-rule="evenodd"
+                              d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                              clip-rule="evenodd"
+                            />
+                          </svg>
+                        </template>
+                      </v-dropdown>
+                    </div>
+                    &lt;!&ndash;drop down ends &ndash;&gt;
+                  </div>
+                </div>
+        -->
       </div>
     </div>
   </header>
